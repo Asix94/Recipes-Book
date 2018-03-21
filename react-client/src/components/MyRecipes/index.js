6fce5ca0b6400d55d8c67ad45bee005533547ef8
Carlos Ramos <31883557+Asix94@users.noreply.github.com>
2018-03-21 18:42:57 +0100

    update CRUD success

--- react-client/src/components/MyRecipes/index.js
+++ react-client/src/components/MyRecipes/index.js
@@ -1,20 +1,68 @@
 import React, { Component } from 'react'
 import api from 'api-client'
+import swal from 'sweetalert2'
 
 class MyRecipes extends Component {
 
-    removeRecipe(id){
+    updateRecipe(id,title,category,image,video,ingredients,elaboration,dificulty,preparation,region,seasson,observation) {
 
         api.protocol = 'http'
         api.host = 'localhost'
         api.port = '5000'
 
-        api.removeRecipe(id)
-        
+        api.updateRecipe(id,title,category,image,video,ingredients,elaboration,dificulty,preparation,region,seasson,observation)
+
     }
 
-    updateRecipe(){
+    swalRecipe(recipe) { 
 
+         swal({
+            title: 'Update Recipe',
+            html:
+                `<input id="title" class="swal2-input" value="${recipe.title}">` +
+                `<input id="category" class="swal2-input" value="${recipe.category}">` +
+                `<input id="dificulty" class="swal2-input" value="${recipe.dificulty}">` +
+                `<input id="preparation" class="swal2-input" value="${recipe.preparation}">` +
+                `<input id="seasson" class="swal2-input" value="${recipe.seasson}">` +
+                `<input id="region" class="swal2-input" value="${recipe.region}">` +
+                `<input id="image" class="swal2-input" value="${recipe.image}">` +
+                `<input id="observation" class="swal2-input" value="${recipe.observation}">` +
+                `<input id="ingredients" class="swal2-input" value="${recipe.ingredients}">` +
+                `<input id="elaboration" class="swal2-input" value="${recipe.elaboration}">`,
+            focusConfirm: false,
+            preConfirm: () => {
+                return {
+                    title: document.getElementById('title').value,
+                    category: document.getElementById('category').value,
+                    dificulty: document.getElementById('dificulty').value,
+                    preparation: document.getElementById('preparation').value,
+                    seasson: document.getElementById('seasson').value,
+                    region: document.getElementById('region').value,
+                    image: document.getElementById('image').value,
+                    observation: document.getElementById('observation').value,
+                    ingredients: document.getElementById('ingredients').value.split(','),
+                    elaboration: document.getElementById('elaboration').value.split(',')
+                }
+            }
+        }).then(res => {
+            this.updateRecipe(
+                recipe._id,
+                res.value.title,
+                res.value.category,
+                res.value.image,
+                undefined,
+                res.value.ingredients,
+                res.value.elaboration,
+                res.value.dificulty,
+                res.value.preparation,
+                res.value.region,
+                res.value.seasson,
+                res.value.observation,
+            )
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
     }
 
     render() {
@@ -35,12 +83,12 @@ class MyRecipes extends Component {
                         <tbody>
                             {this.props.recipe.map((recipe, index) => {
                                 return (
-                                    <tr>
+                                    <tr key={index}>
                                         <td>{recipe.title}</td>
                                         <td>{recipe.category}</td>
                                         <td>{recipe.dificulty}</td>
-                                        <td><button className="btn">Update</button></td>
-                                        <td><button className="btn" type="submit" onClick={e => {e.preventDefault(); this.removeRecipe(recipe._id)}}>Remove</button></td>
+                                        <td><button className="btn" type="submit" onClick={e => { e.preventDefault(); this.swalRecipe(recipe) }}>Update</button></td>
+                                        <td><button className="btn" type="submit" onClick={e => { e.preventDefault(); this.removeRecipe(recipe._id) }}>Remove</button></td>
                                     </tr>
                                 )
                             })}
